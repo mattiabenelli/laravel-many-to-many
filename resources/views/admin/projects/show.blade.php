e907ee15a83b269f8b8979fa9d224ad04a6d9e9a
mattiabenelli <benellimattia0@gmail.com>
2023-03-08 15:57:07 +0100

    show tecnologia done

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -7,6 +7,13 @@
                     <h1 class="my-4">{{ $project->title }}</h1>
                     <h2>Categoria:</h2>
                     <p>{{ $project->type ?  $project->type->name : 'Senza categoria'}}</p>
+                    <h2>Tecnologie utilizzate:</h2>
+                        @forelse ($project->technologies as $item)
+                            <div>{{ $item->name }}</div>
+                            @empty
+                            nessuna tecnologia associata al post
+
+                        @endforelse
                     <h2>Contenuto:</h2>
                     <p>{{ $project->content }}</p>
                     <a href="{{ route('admin.projects.index') }}">Torna all'elenco</a>
